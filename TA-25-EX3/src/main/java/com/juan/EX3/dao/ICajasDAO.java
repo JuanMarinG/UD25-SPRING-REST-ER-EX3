4fb402d81c7aa966b3614e432e3e3edc0a1b06a8
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-08-22 11:31:53 +0200

    Update ICajasDAO.java

--- TA-25-EX3/src/main/java/com/juan/EX3/dao/ICajasDAO.java
+++ TA-25-EX3/src/main/java/com/juan/EX3/dao/ICajasDAO.java
@@ -1,5 +1,8 @@
 package com.juan.EX3.dao;
 
-public interface ICajasDAO {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.juan.EX3.dto.Cajas;
+
+public interface ICajasDAO extends  JpaRepository<Cajas, Long>{
 
 }
